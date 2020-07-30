fa34bf77ff4c1e99f95ca5050e15f16d6bb3cc0c
Praveen Gundu <praveenkumardec89@gmail.com>
2020-07-30 14:45:47 +0530

    Update Accesstokens.java

--- src/com/ibm/abn/hackathon/bot/interfaces/Accesstokens.java
+++ src/com/ibm/abn/hackathon/bot/interfaces/Accesstokens.java
@@ -3,8 +3,8 @@ package com.ibm.abn.hackathon.bot.interfaces;
 public interface Accesstokens {
 		
 												   
-	public static final String page_access_token="EAAKMWaVn2d4BAJkVuQD2aTatC4UzZACBxQZB2qJZA2wNK1ZAAsGMoihidhsno11eiZCmJsywdjMws1IksLB0Tp9bu4kLL5SyZCWe8LI1ouoYI7cC400BE3cAWVMJCLQqrCgm3Uxc1U3Bz38nr99PWZBJXNB0il0Vl6PRx5Ajt8ZBvwZDZD";
-	public static final String page_receipent_token="1348367891916496";
+	public static final String page_access_token="XXXXX";
+	public static final String page_receipent_token="111111";
 	public static final String fb_postback_url="https://graph.facebook.com/v2.6/me/messages?access_token=";
 	public static final String url= fb_postback_url+page_access_token;
 	public static final String urlForGettingProfilepart1= "https://graph.facebook.com/v2.6/";
